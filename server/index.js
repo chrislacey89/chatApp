5852f7cf1979bb63839218be6160deb1742423da
Chris Lacey <ChrisLacey89@gmail.com>
2020-05-16 13:05:44 -0400

    inital set up of socket io

--- server/index.js
+++ server/index.js
@@ -5,8 +5,20 @@ const http = require('http');
 // deployment will require process.env.port
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('We have a new connection!');
+
+  socket.on('disconnect', () => {
+    console.log('User has left!');
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
