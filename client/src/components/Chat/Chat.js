816fdb2881a735714f33c260fa992f05206dcd53
Chris Lacey <ChrisLacey89@gmail.com>
2020-05-19 19:57:29 -0400

    connect socket to chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import querystring from 'query-string';
+import io from 'socket.io-client';
 
-function Chat() {
+let socket;
+
+function Chat({ location }) {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = querystring.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
+
   return <h1>Chat</h1>;
 }
 
